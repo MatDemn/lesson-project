cba2df9b1ae2383f86899e243254958b03bc2329
MatZaj <mat.zaj.work@gmail.com>
2024-08-03 22:59:00 +0200

    Header revamped

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,24 +6,29 @@ import { useEffect, useState } from "react";
 import {getDiscordImage} from '../utils/fetchDiscordImage';
 import UserCard from "./partials/userCard";
 import LoggedOutCard from "./partials/loggedOutCard";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import FaButton from "./singleComponents/faButton";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDumbbell, faCalendar, faSearch, faAddressCard, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faDumbbell, faCalendar, faSearch, faAddressCard, faBars, faX } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from "react-router-dom";
 
 interface HeaderProps {
   loggedUser: DiscordUser | null,
-  onLoginClicked: () => void,
   onLogoutSuccessful: () => void
 }
 
-const Header = ({loggedUser, onLoginClicked, onLogoutSuccessful} : HeaderProps) => {
+const Header = ({loggedUser, onLogoutSuccessful} : HeaderProps) => {
 
   const [hamburgerMenuActive, setHamburgerMenuActive] = useState(false);
 
   function switchHamburgerMenu() {
     setHamburgerMenuActive(!hamburgerMenuActive);
+    if(hamburgerMenuActive) {
+      document.body.style.overflow = "";
+    }
+    else {
+      document.body.style.overflow = "hidden";
+    }
   }
 
   let navigate = useNavigate();
@@ -35,7 +40,7 @@ const Header = ({loggedUser, onLoginClicked, onLogoutSuccessful} : HeaderProps) 
     <>
       <div className={styles.mainContainer}>
         <div className={styles.mainBar}>
-          <div className={styles.logoBrandContainer} onClick={() => routeChange("/")}>
+          <div className={styles.logoBrandContainer} onClick={() => {routeChange("/"); if(hamburgerMenuActive) {switchHamburgerMenu()}}}>
             Lesson Project
           </div>
           <div className={styles.navigationBar+" d-none d-xl-block"} >
@@ -53,19 +58,33 @@ const Header = ({loggedUser, onLoginClicked, onLogoutSuccessful} : HeaderProps) 
                   user={loggedUser} 
                   onLogoutSuccessful={onLogoutSuccessful}
                 />
-              : <LoggedOutCard onLoginClicked={onLoginClicked} />
+              : <LoggedOutCard />
             }
           </div>
           <div className={styles.hamburgerButton+" d-xl-none"}>
             <FaButton iconDef={faBars} content="" faSize="2x" onClick={switchHamburgerMenu}/>
           </div>
           {hamburgerMenuActive &&
-            <div className={styles.hamburgerMenu+" d-xl-none"}>
+            <div className={styles.hamburgerMenu+" d-xl-none position-fixed"}>
+              <Button 
+                className={styles.hamburgerClose + " bg-dark border-light"}
+                onClick={() => {switchHamburgerMenu();}}
+              ><FontAwesomeIcon icon={faX}/></Button>
               <ul>
-                <li><FaButton iconDef={faDumbbell} className={navButtonStyles.navButton+" "+styles.hamburgerMenuItem} content="Zadania" faSize="2x"  onClick={() => routeChange("/exercises")}/></li>
-                <li><FaButton iconDef={faCalendar} className={navButtonStyles.navButton+" "+styles.hamburgerMenuItem} content="Kalendarz" faSize="2x" onClick={() => routeChange("/calendar")}/></li>
-                <li><FaButton iconDef={faAddressCard} className={navButtonStyles.navButton+" "+styles.hamburgerMenuItem} content="O mnie" faSize="2x" onClick={() => routeChange("/about")}/></li>
-                <li><FaButton iconDef={faSearch} className={navButtonStyles.navButton+" "+styles.hamburgerMenuItem} content="Szukaj" faSize="2x" onClick={() => routeChange("/search")}/></li>
+                <li><FaButton iconDef={faDumbbell} className={navButtonStyles.navButton+" "+styles.hamburgerMenuItem} content="Zadania" faSize="2x"  onClick={() => {routeChange("/exercises"); switchHamburgerMenu();}}/></li>
+                <li><FaButton iconDef={faCalendar} className={navButtonStyles.navButton+" "+styles.hamburgerMenuItem} content="Kalendarz" faSize="2x" onClick={() => {routeChange("/calendar"); switchHamburgerMenu();}}/></li>
+                <li><FaButton iconDef={faAddressCard} className={navButtonStyles.navButton+" "+styles.hamburgerMenuItem} content="O mnie" faSize="2x" onClick={() => {routeChange("/about"); switchHamburgerMenu();}}/></li>
+                <li><FaButton iconDef={faSearch} className={navButtonStyles.navButton+" "+styles.hamburgerMenuItem} content="Szukaj" faSize="2x" onClick={() => {routeChange("/search"); switchHamburgerMenu();}}/></li>
+                <li>
+                {
+                  loggedUser ? 
+                    <UserCard 
+                      user={loggedUser} 
+                      onLogoutSuccessful={onLogoutSuccessful}
+                    /> :
+                   <LoggedOutCard />
+                }
+                </li>
               </ul>
             </div>
           }
